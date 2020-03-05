4225694942b6c810bf9815ac2079e2a03caf1af8
josemariaallegue <josemariaallegue@users.noreply.github.com>
2020-03-05 17:44:53 -0300

    Ejercicicos al 05/03/2020

---
+++ Practicas/Wpf_demo/Persona.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wpf_demo
+{
+    class Persona
+    {
+        #region campos
+
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Nacionalidad { get; set; }
+        public int Edad { get; set; }
+
+        #endregion
+
+        #region constructores
+
+        public Persona(string nombre, string apellido, string nacionalidad, int edad)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            Nacionalidad = nacionalidad;
+            Edad = edad;
+        }
+
+        #endregion
+
+        #region metodos
+
+        public string Mostrar()
+        {
+            StringBuilder retorno = new StringBuilder();
+
+            retorno.Append($"Nombre: {Nombre}").AppendLine();
+            retorno.Append($"Apellido: {Apellido}").AppendLine();
+            retorno.Append($"Nacionalidad: {Nacionalidad}").AppendLine();
+            retorno.Append($"Edad: {Edad}").AppendLine();
+
+            return retorno.ToString();
+        }
+
+        #endregion
+
+    }
+}
